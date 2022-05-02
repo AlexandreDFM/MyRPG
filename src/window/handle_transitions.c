3349a26bbbd1d6ff0facb04b55e92d32689e5aa9
Quentin Briand <quentin.briand@epitech.eu>
2022-05-02 15:22:25 +0200

    TestTransitions

--- src/window/handle_transitions.c
+++ src/window/handle_transitions.c
@@ -21,10 +21,11 @@ void update_transition(wininf *inf, player p)
             sfRectangleShape_setFillColor(inf->transi, col);
             return;
         }
+        printf("%f (%f)\n", new_a, inf->time.dt);
         new_a += inf->time.dt;
-        printf("%f\n", new_a);
+        printf("\t%f\n", new_a);
     } else {
-        if (new_a <= 0.05f) {
+        if (new_a <= inf->time.dt) {
             inf->transition = 0; inf->time.transi = 0.0f;
         }
         new_a -= inf->time.dt * 3.0f;
