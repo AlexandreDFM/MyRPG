afbd1f618852502db3adb4b6dfb66427c2c94ffb
Quentin Briand <quentin.briand@epitech.eu>
2022-05-04 00:56:50 +0200

    Coding styled camera_follow.c

--- src/camera/camera_follow.c
+++ src/camera/camera_follow.c
@@ -9,16 +9,12 @@
 
 void update_camera(wininf *inf)
 {
-    if (!inf->camera.target) {
+    if (!inf->camera.target)
         return;
-    }
     sfVector2f pos = sfSprite_getPosition(inf->camera.target);
-    // sfVector2f size = sfView_getSize(inf->camera.view);
-    // pos.x = my_clamp(pos.x, size.x / 2.0f, max_x - size.x / 2.0f);
-    // pos.y = my_clamp(pos.y, size.y / 2.0f, max_y - size.y / 2.0f);
+    sfVector2f center = sfView_getCenter(inf->camera.view);
     if (inf->dungeon.in)
-        pos = my_lerp(sfView_getCenter(inf->camera.view), pos,
-        6.0f * inf->time.dt);
+        pos = my_lerp(center, pos, 6.0f * inf->time.dt);
     sfView_setCenter(inf->camera.view, pos);
     sfRectangleShape_setPosition(inf->transi, pos);
     sfRenderWindow_setView(inf->win, inf->camera.view);
