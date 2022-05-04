7cbba05c044747eb04f66fb26f31106146fac18b
AlexandreDFM <alexandre-kevin.de-freitas-martins@epitech.eu>
2022-05-04 03:08:20 +0200

    [~] Coding Style Dream

--- src/dream/create_dream.c
+++ src/dream/create_dream.c
@@ -12,12 +12,6 @@ dream_a *create_dream(wininf *inf)
 {
     dream_a *dream = malloc(sizeof(dream_a));
     dream->plan = DREAMINIT;
-    // dream->b_dream[0] = c_eles("dreams_1.png", (sfVector2f) {0, 0}, (sfIntRect) {0, 0, 882 / 3, 384});
-    // dream->b_dream[0].resize = (sfVector2f) {6.530612245, 5.625};
-    // dream->b_dream[1] = c_eles("dreams_1.png", (sfVector2f) {0, 0}, (sfIntRect) {882 / 3, 0, 882 / 3, 384});
-    // dream->b_dream[1].resize = (sfVector2f) {6.530612245, 5.625};
-    // dream->b_dream[2] = c_eles("dreams_1.png", (sfVector2f) {0, 0}, (sfIntRect) {882 / 3 + 882 / 3, 192, 882 / 3, 384});
-    // dream->b_dream[2].resize = (sfVector2f) {6.530612245, 5.625};
     for (int i = 0, j = 1; i < 3; i++, j++) {
         dream->b_dream[i] = c_ele(inf->atlases.atlas, "./csv/dreams.csv", j);
     }
