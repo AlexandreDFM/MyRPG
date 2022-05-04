7d7b13cd66272ab975bbec698e730cfc25d63b65
AlexandreDFM <alexandre-kevin.de-freitas-martins@epitech.eu>
2022-05-04 22:42:22 +0200

    [U] Update Quiz, new system, all completed, can choice the answer & respond to

--- include/quiz.h
+++ include/quiz.h
@@ -6,6 +6,7 @@
 */
 
 #include "rpg.h"
+#include "cinematics_elements.h"
 
 #ifndef QUIZ_H_
     #define QUIZ_H_
@@ -24,29 +25,42 @@ enum nature {
     Relaxed = 10,
     Lonely = 11,
     Quirky = 12,
+    HOMME = 13,
+    FEMELLE = 14
 };
 
-enum sexe {
-    MALE = 0,
-    FEMELLE = 1,
-};
+typedef struct questions_quiz {
+    sfText *question;
+    sfText *answer1;
+    int ptsanswer1;
+    sfText *answer2;
+    int ptsanswer2;
+    struct questions_quiz *next;
+} q_quiz;
 
 typedef struct quiz_t {
-    int sexe;
+    int clicked;
     int destroy;
     int tendance;
     int nbquestions;
     int actualquestion;
-    char **aquestions;
     int *nature;
     int *addptsnature;
     char **questions;
+    char **aquestions;
+    cine_ele cursor;
     sfSprite *quizbox;
     sfText *text;
+    q_quiz *questions_quiz;
+    q_quiz *actualquestion_quiz;
 } quiz;
 
+void handle_quiz(wininf *inf);
 void delete_quiz(wininf *inf);
+void quiz_choice(wininf *inf);
 quiz *create_quiz(wininf *inf);
 int chose_line(char **csv, int tendance, int sexe);
+void move_cursor_quiz(wininf *inf, cine_ele *cursor);
+q_quiz *create_questions_quiz(wininf *inf, char **csv, int nbquestions);
 
 #endif /* !QUIZ_H_ */
