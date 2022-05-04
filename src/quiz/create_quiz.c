2ca642c48ad23c2c33afb93edb44b150174f3753
AlexandreDFM <alexandre-kevin.de-freitas-martins@epitech.eu>
2022-05-04 04:19:55 +0200

    [+] New CSV with questions -> Need to modif the give pts, Creation of quiz with all the csv file

--- src/quiz/create_quiz.c
+++ src/quiz/create_quiz.c
@@ -10,7 +10,7 @@
 quiz *create_quiz(wininf *inf)
 {
     quiz *qcm = malloc(sizeof(quiz));
-    qcm->destroy = 0; qcm->tendance = 0;
+    qcm->destroy = 0; qcm->tendance = 0; qcm->nbquestions = 0;
     qcm->nature = malloc(sizeof(int) * 13);
     qcm->addptsnature = malloc(sizeof(int) * 13);
     for (int i = 0; i < 13; i++) {
@@ -18,8 +18,7 @@ quiz *create_quiz(wininf *inf)
     }
     for (int i = 0; i < 13; i++) qcm->nature[i] = 0;
     qcm->questions = load_csv("./csv/quiz_questions.csv");
-    qcm->nbquestions = 6;
-    // for (; qcm->questions[qcm->nbquestions] != NULL; qcm->nbquestions++);
+    for (; qcm->questions[qcm->nbquestions] != NULL; qcm->nbquestions++);
     qcm->actualquestion = 1; qcm->aquestions = NULL;
     qcm->quizbox = generate_textbox((sfVector2i) {385, 50}, inf->atlases.atlas);
     sfSprite_setPosition(qcm->quizbox, (sfVector2f) {200, 720});
